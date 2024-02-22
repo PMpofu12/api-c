9b6ea4671a317cc88c45e9a2fac446ca199e8cad
Philani Mpofu <49895156+PMpofu12@users.noreply.github.com>
2024-02-22 15:44:32 +0200

    broke up intial template into appropriate modules

---
+++ pkg/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/api-prices/pkg/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRouter() {
+	Router := gin.Default()
+
+	Router.GET("/albums", handler.GetAlbums)
+	Router.GET("/albums/:id", handler.GetAlbumByID)
+	Router.POST("/albums", handler.PostAlbum)
+}
