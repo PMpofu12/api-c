9fa35cc13c8907bf2b9e754b05e0bbf50f1a0d62
Philani Mpofu <49895156+PMpofu12@users.noreply.github.com>
2024-02-24 23:39:28 +0200

    added new fields to cryptocurrency struct

--- pkg/model/cryptocurrency.go
+++ pkg/model/cryptocurrency.go
@@ -5,7 +5,12 @@ type CryptocurrencyList struct {
 }
 
 type Cryptocurrency struct {
-	ID     string `json:"id"`
-	Symbol string `json:"symbol"`
-	Name   string `json:"name"`
+	ID           string `json:"id"`
+	Symbol       string `json:"symbol"`
+	Name         string `json:"name"`
+	CurrentPrice int    `json:"current_price"`
+	MarketCap    int    `json:"market_cap"`
+	TotalVolume  int    `json:"total_volume"`
+	High24h      int    `json:"24h_high"`
+	Low24h       int    `json:"24_low"`
 }
